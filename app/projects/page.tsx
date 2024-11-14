796e85d8fceabae6dd91af055edce3c261ed5e95
irfazbow <irfanefp@gmail.com>
2024-11-14 16:34:41 +0700

    fix: draggable button for nav side limited by container, now moved outside

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,24 +9,26 @@ const Projects = () => {
   ];
 
   return (
-    <div className="min-h-screen max-w-[1536px] mt-20 mx-auto">
+    <>
       <DraggableNavButton
         navigationItems={projectNavigationItems}
         navigationName="About"
       />
-      <div className="flex flex-col gap-8 py-12 px-4">
-        <div id="outerwilds">
-          <OuterWilds />
-        </div>
-        <div className="flex flex-row justify-between">
-          <div className="h-[3px] w-[200px] sm:w-[400px] bg-primary-orange my-10 bg-opacity-60"></div>
-          <div className="h-[3px] w-[50px] sm:w-[150px] bg-primary-orange my-10 bg-opacity-60"></div>
-        </div>
-        <div id="nginep">
-          <Nginep />
+      <div className="min-h-screen max-w-[1536px] mt-20 mx-auto">
+        <div className="flex flex-col gap-8 py-12 px-4">
+          <div id="outerwilds">
+            <OuterWilds />
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="h-[3px] w-[200px] sm:w-[400px] bg-primary-orange my-10 bg-opacity-60"></div>
+            <div className="h-[3px] w-[50px] sm:w-[150px] bg-primary-orange my-10 bg-opacity-60"></div>
+          </div>
+          <div id="nginep">
+            <Nginep />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
