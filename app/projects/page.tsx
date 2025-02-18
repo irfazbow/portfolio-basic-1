e435a0ccabafaf75f1e739c47b57e6a4bc0ee63c
irfazbow <irfanefp@gmail.com>
2025-02-18 17:58:55 +0700

    fix: change navigation name on project page from about to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
     <>
       <DraggableNavButton
         navigationItems={projectNavigationItems}
-        navigationName="About"
+        navigationName="Projects"
       />
       <div className="min-h-screen max-w-[1536px] mt-20 mx-auto">
         <div className="flex flex-col gap-8 py-12 px-4">
